1ae3912858bae6c819ce1c7dc5780ea12829ebce
ezacoantonio <ezacoantonio@gmail.com>
2023-09-19 21:36:32 -0400

    Chapter 2 end training

--- Training/server.js
+++ Training/server.js
@@ -1,10 +1,34 @@
-const http = require('http');
+// const http = require('http');
 
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello World');
-}).listen(3000);
+// http.createServer((req, res) => {
+//     res.writeHead(200, {
+//         'Content-Type': 'text/plain'
+//     });
+//     res.end('Hello World');
+// }).listen(3000);
 
+// console.log('Server running at http://localhost:3000/');
+
+
+const express = require('express');
+const app = express();
+
+// Middleware 1: Logging
+app.use((req, res, next) => {
+  console.log('This is a logging middleware');
+  next();
+});
+
+// Middleware 2: Authentication
+app.use((req, res, next) => {
+  console.log('This is an authentication middleware');
+  next();
+});
+
+// Final Route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3000);
 console.log('Server running at http://localhost:3000/');
