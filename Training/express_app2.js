9b142fef10a47ca9904244a45bcc11a65baae1bb
ezacoantonio <ezacoantonio@gmail.com>
2023-09-20 19:17:23 -0400

    Update express_app2.js

--- Training/express_app2.js
+++ Training/express_app2.js
@@ -1,11 +1,17 @@
 const connect = require ('connect');
 const app = connect();
 
+function logger(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+}
+
 function helloWorld(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.end('hello World!');
 };
 
+app.use(logger);
 app.use(helloWorld);
 
 app.listen(3000);
